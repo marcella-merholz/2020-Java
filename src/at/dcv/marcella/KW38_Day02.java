51303e40427790a554d20aa9874e3a9ef2b6967f
marcella-merholz <marcella_merholz@web.de>
2020-11-04 13:02:57 +0100

    review

--- src/at/dcv/marcella/KW38_Day02.java
+++ src/at/dcv/marcella/KW38_Day02.java
@@ -3,7 +3,6 @@ package at.dcv.marcella;
 public class KW38_Day02 {
 
     public static void bool() {
-
         int schoki = 1;
         boolean schokiKaufen = schoki <= 2;
         if (schokiKaufen) {
@@ -18,8 +17,24 @@ public class KW38_Day02 {
         } else System.out.println("keine Schuhe kaufen");
     }
 
+
+    // while und for-Schleife
+    public static void loops() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            ++i;
+        }
+
+        int j = 0;
+        for (j = 0; j < 10; ++j) {
+            System.out.println(j);
+        }
+    }
+
+
+    // Pi-Annäherung  4 - 4/3 + 4/5 - 4/7 + 4/9 ...
     public static void pi() {
-        // Pi-Annäherung  4 - 4/3 + 4/5 - 4/7 + 4/9 ...
         double PiZahl = 4.0;
         System.out.println(PiZahl);
         PiZahl -= 4.0 / 3;
@@ -36,24 +51,9 @@ public class KW38_Day02 {
         System.out.println(PiZahl);
         PiZahl -= 4.0 / 15;
         System.out.println(PiZahl);
-    }
 
-    public static void loops() {
-    // while und for-Schleife
-
-        int i = 0;
-        while (i < 10) {
-            System.out.println(i);
-            ++i;
-        }
-
-        int j = 0;
-        for (j = 0; j < 10; ++j) {
-            System.out.println(j);
-        }
 
         // Pi-Annäherung mit for-Schleife
-
         int nenner = 3;
         double calcPi = 4.0;
 
@@ -62,42 +62,37 @@ public class KW38_Day02 {
             nenner += 2;
             calcPi += 4.0 / nenner;
             nenner += 2;
-
             System.out.println(calcPi);
         }
     }
 
+    // Summe bis 100 ausrechnen 1 + 2 + 3 + 4 ... 100
     public static void sum() {
-// Summe bis 100 ausrechnen 1 + 2 + 3 + 4 ... 100
         int summe = 0;
-
         for (int i = 0; i <= 100; i++) {
-            summe = summe + i;
+            summe += i;
             System.out.println(summe);
         }
     }
 
+    // multiplizieren 1 * 2 * 3 ... 20
     public static void multiplyFor() {
-// multiplizieren 1 * 2 * 3 ... 20
-
         long multiplikation = 1;
-
         for (int i = 1; i <= 20; i++) {
-            multiplikation = multiplikation * i;
+            multiplikation *= i;
             System.out.println(multiplikation);
         }
     }
 
+    // Quadrat aus Buchstaben A erstellen mit variabler Anzahl an Zeilen und Buchstaben pro Zeile
     public static void rowColumnOutput() {
-// Quadrat aus Buchstaben A erstellen mit variabler Anzahl an Zeilen und Buchstaben pro Zeile
-
         String text = "A";
-
         for (int size = 1; size <= 5; size++) {
-            for (int i = 1; i <= 5; i++) {
+            for (int i = 1; i <= 7; i++) {
                 System.out.print(text);
             }
             System.out.println();
         }
     }
+
 }
