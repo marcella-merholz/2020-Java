35e401da741a5c27e63e01e5b09cadefce18a411
marcella-merholz <marcella_merholz@web.de>
2020-11-04 14:02:13 +0100

    review

--- src/at/dcv/marcella/KW40_Day01.java
+++ src/at/dcv/marcella/KW40_Day01.java
@@ -3,13 +3,13 @@ package at.dcv.marcella;
 public class KW40_Day01 {
 
     public static void businessResultForEach() {
-        // Aufgabe Suche nach: zwei hintereinander kommende Monate mit negativen Ergebnis?
+        // Aufgabe Suche nach: zwei hintereinander kommenden Monaten mit negativen Ergebnis?
         // Lösung mit for-each-Schleife
         int[] businessResult = {1, 2, -5, -3, 10, -2, 1, -3, 0, 3, 6, 7};
         boolean wasNegative = false;
-        for (int result : businessResult) {     // for-each-Schleife: Datentyp definieren (Zahlen, String ...) : Array-Name
+        for (int result : businessResult) { // for-each-Schleife: Datentyp definieren (Zahlen, String ...) : Array-Name
             if (result < 0) {
-                if (wasNegative) {  // bei der ersten Zahl < 0 läuft die Schleife noch weiter, da wasNegative mit false initialisiert
+                if (wasNegative) { // bei der ersten Zahl < 0 läuft die Schleife noch weiter, da wasNegative mit false initialisiert
                     System.out.println("2 oder mehr Monate mit negativem Ergebnis");
                     break;
                 } else {
@@ -25,10 +25,10 @@ public class KW40_Day01 {
         for (int result : businessResult) {
             if (result < 0) {
                 negCounter++;
-                if (interval < negCounter) {    // wichtig! die Übergabe muss an dieser Stelle* stattfinden, ansonsten falsch, wenn das negative Intervall am Ende des Arrays
+                if (interval < negCounter) { // wichtig! die Übergabe muss an dieser Stelle* stattfinden, ansonsten falsch, wenn das negative Intervall am Ende des Arrays
                     interval = negCounter;
                 }
-            } else {    // *nicht hier
+            } else { // *nicht hier
                 negCounter = 0;
             }
         }
@@ -40,7 +40,7 @@ public class KW40_Day01 {
             if (result >= maxValue) {
                 secMaxValue = maxValue;
                 maxValue = result;
-            } else if (result >= secMaxValue) {  // für den Fall, dass der zweitgrößte Wert nach dem größten Wert kommt. dann trifft die erste if-Bedingung nicht zu.
+            } else if (result >= secMaxValue) { // für den Fall, dass der zweitgrößte Wert nach dem größten Wert kommt. dann trifft die erste if-Bedingung nicht zu.
                 secMaxValue = result;
             }
         }
