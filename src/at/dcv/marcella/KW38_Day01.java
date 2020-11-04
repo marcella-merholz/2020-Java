22cb08f6abe1385a4b3079b2e0450d68421fdf95
marcella-merholz <marcella_merholz@web.de>
2020-11-04 12:46:01 +0100

    Anweisungen kommentiert

--- src/at/dcv/marcella/KW38_Day01.java
+++ src/at/dcv/marcella/KW38_Day01.java
@@ -3,11 +3,12 @@ package at.dcv.marcella;
 public class KW38_Day01 {
 
     public static void dataTypes() {
-
         System.out.println("Hello World");
-
         char buchstabe = 'M';
         float nochEinFloat = 02.0F;
+        System.out.println(buchstabe);
+        System.out.println(nochEinFloat);
+        System.out.println(buchstabe + nochEinFloat); // !!! hier wird char in float? verwandelt (ASCII-Code Belegung: 77)
     }
 
     public static void sortInstruction () {
@@ -15,11 +16,11 @@ public class KW38_Day01 {
         int neueZahl = 7;
         System.out.println(++neueZahl); // zählt hoch, gibt dann aus
         System.out.println(neueZahl++); // gibt aus, zählt dann hoch
-        System.out.println(neueZahl * neueZahl);
-        System.out.println(neueZahl = neueZahl * neueZahl);
-        System.out.println(neueZahl=(neueZahl-9) / 8);
-        System.out.println(neueZahl--);
-        System.out.println(--neueZahl);
-        System.out.println(neueZahl);
+        System.out.println(neueZahl * neueZahl); // neue Zahl ist hier immer noch 9
+        System.out.println(neueZahl = neueZahl * neueZahl); // jetzt ist neue Zahl 81
+        System.out.println(neueZahl = (neueZahl -9) / 8); // 72 / 8 = 9
+        System.out.println(neueZahl--); // gibt erst aus, zählt dann runter
+        System.out.println(--neueZahl); // zählt runter, gibt dann aus
+        System.out.println(neueZahl); // gibt nur aus
     }
 }
