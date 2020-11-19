8655d4c18b53247322a8d313c0105634efc07186
marcella-merholz <marcella_merholz@web.de>
2020-11-19 23:11:48 +0100

    review

--- src/at/dcv/marcella/KW47_Day01_BudgetFK19.java
+++ src/at/dcv/marcella/KW47_Day01_BudgetFK19.java
@@ -13,8 +13,17 @@ public class KW47_Day01_BudgetFK19 {
         System.out.println(clearBudget.length);
 
         int sumFK19 = getSumClearBudget(clearBudget);
-        System.out.println(sumFK19);
+        System.out.println("Die Summe aller Ausgaben beträgt: " + sumFK19 + "\n");
 
+        String[][] sortedBudget = getSortedArray(clearBudget);
+        printTopMax(sortedBudget, 10);
+        printTopMin(sortedBudget, 20);
+
+        String[] bereich = {"Volksschule", "Volksschule", "Mittelschule", "Polytechnische"};
+        int anteilBereich = getAnteilBereich(clearBudget, bereich);
+        getAnteilPerCent(clearBudget, bereich[2], sumFK19, anteilBereich);
+        int anteilSchulen = getAnteilSchulen(clearBudget);
+        int anteilKindergarten = getAnteilKindergarten(clearBudget);
     }
 
     private static String[][] getClearBudget(String[][] budgetFK19) {
@@ -59,7 +68,80 @@ public class KW47_Day01_BudgetFK19 {
         return sumBudget;
     }
 
+    public static String[][] getSortedArray(String[][] arrayToSort) {
+        String[][] copyArray = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        for (int i = 0; i < copyArray.length; ++i) {
+            for (int j = 0; j < copyArray.length - 1 - i; ++j) { // -i verkürzt hier den Zugriff, da die letzten Zahlen bereits sortiert sind
+                if (Integer.parseInt(copyArray[j][2]) < Integer.parseInt(copyArray[j + 1][2])) {
+                    String[] temp = copyArray[j];
+                    copyArray[j] = copyArray[j + 1];
+                    copyArray[j + 1] = temp;
+                }
+            }
+        }
+        return copyArray;
+    }
 
+    public static void printTopMax(String[][] sortedBudget, int range) {
+        System.out.println("Die " + range + " höchsten Ausgaben waren: ");
+        for (int i = 0; i < range; i++) {
+            System.out.println(Arrays.toString(sortedBudget[i]));
+        }
+        System.out.println("\n");
+    }
+
+    public static void printTopMin(String[][] sortedBudget, int range) {
+        System.out.println("Die " + range + " niedrigsten Ausgaben waren: ");
+        for (int i = sortedBudget.length - range; i < sortedBudget.length; i++) {
+            System.out.println(Arrays.toString(sortedBudget[i]));
+        }
+        System.out.println("\n");
+    }
+
+    private static int getAnteilBereich(String[][] clearBudget, String[] bereich) {
+        int anteilBereich = 0;
+        for (int i = 0; i < clearBudget.length; i++) {
+            for (int j = 0; j < bereich.length; j++) {
+                if (clearBudget[i][0].contains(bereich[j])) {
+                    anteilBereich += Integer.parseInt(clearBudget[i][2]);
+                    break;
+                }
+            }
+        }
+        System.out.println("Der Anteil der Ausgaben für den ausgewählten Bereich beträgt; " + anteilBereich);
+        return anteilBereich;
+    }
+
+    private static double getAnteilPerCent(String[][] clearBudget, String bereich, int sumFK19, int anteilBereich) {
+        float anteilPerCent = (anteilBereich / (float) sumFK19 * 100);
+        System.out.println("Der Anteil des ausgewählten Bereiches an den gesammten Ausgaben in Prozent ist: " + String.format("%.2f", anteilPerCent) + "%.");
+        return anteilPerCent;
+    }
+
+    private static int getAnteilSchulen(String[][] clearBudget) {
+        int anteilSchulen = 0;
+        for (int i = 0; i < clearBudget.length; i++) {
+            if (clearBudget[i][0].contains("Volksschule") ||
+                    clearBudget[i][0].contains("Mittelschule") ||
+                    clearBudget[i][0].contains("Polytechnische")) {
+                anteilSchulen += Integer.parseInt(clearBudget[i][2]);
+            }
+        }
+        System.out.println("Der Anteil der Ausgaben für Schulen beträgt; " + anteilSchulen);
+        return anteilSchulen;
+    }
+
+    private static int getAnteilKindergarten(String[][] clearBudget) {
+        int anteilKindergarten = 0;
+        for (int i = 0; i < clearBudget.length; i++) {
+            if (clearBudget[i][0].contains("Kindergarten") ||
+                    clearBudget[i][0].contains("Ganztagskindergarten")) {
+                anteilKindergarten += Integer.parseInt(clearBudget[i][2]);
+            }
+        }
+        System.out.println("Der Anteil der Ausgaben für Kindergärten beträgt: " + anteilKindergarten);
+        return anteilKindergarten;
+    }
 
 
 }
